b97c39391ca3e04c5f87fa1a5c3686749a624939
Adam Titus and Rane Fields <adam+rane@>
2017-10-31 10:00:21 -0700

    All basic course and students tests pass.

--- Registrar.Solution/Registrar.Tests/Models.Tests/Course.Tests.cs
+++ Registrar.Solution/Registrar.Tests/Models.Tests/Course.Tests.cs
@@ -25,5 +25,31 @@ namespace Registrar.Models.Tests
       Course epicodusS = new Course("CSharp", "C#");
       Assert.AreEqual(epicodus, epicodusS);
     }
+    [TestMethod]
+    public void GetAll_CheckDatabaseEmpty_0()
+    {
+      int result = Course.GetAll().Count;
+
+      Assert.AreEqual(0, result);
+    }
+    [TestMethod]
+    public void Save_SaveCourseToDatabase_CourseList()
+    {
+      Course epicodus = new Course("CSharp", "C#");
+      epicodus.Save();
+
+      List<Course> result = Course.GetAll();
+      List<Course> testList = new List<Course>{epicodus};
+
+      CollectionAssert.AreEqual(testList, result);
+    }
+    [TestMethod]
+    public void Find_FindsCourseInDatabase_Course()
+    {
+      Course epicodus = new Course("CSharp", "C#");
+      epicodus.Save();
+
+      Assert.AreEqual(epicodus, Course.Find(epicodus.Id));
+    }
   }
 }
